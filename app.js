dc64655bb032dff0d6ecdcf0a74e1606089b5f91
Neil Hong <807312043@qq.com>
2022-11-09 16:57:53 +1100

    change to script, complete sorting data

--- app.js
+++ app.js
@@ -1,21 +1,62 @@
-const express = require("express");
-const https = require("https");
+// const express = require("express");
+// const https = require("https");
+const axios = require("axios");
 
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", function (req, res) {
-    let data = "";
-    https.get("https://api.publicapis.org/entries", function (response) {
-        response.on("data", function (chunk) {
-            data += chunk;
-            console.log(data);
+axios
+    .get("https://api.publicapis.org/entries")
+    .then((response) => {
+        let fetchedData = [];
+        response.data.entries.map((item) => {
+            fetchedData.push(item);
         });
-    });
+        fetchedData.sort((a, b) => {
+            const api1 = a.API.toUpperCase();
+            const api2 = b.API.toUpperCase();
+            if (api1 < api2) {
+                return -1;
+            }
+            if (api1 > api2) {
+                return 1;
+            }
+            return 0;
+        });
+        console.log(fetchedData);
+    })
+    .catch((err) => console.log(err));
 
-    res.send("Server is up and runnig");
-});
+// const app = express();
+// app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000");
-});
+// app.get("/", function (req, res) {
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+// app.post("/", function (req, res) {
+//     axios
+//         .get("https://api.publicapis.org/entries")
+//         .then((response) => {
+//             let fetchedData = [];
+//             response.data.entries.map((item) => {
+//                 fetchedData.push(item);
+//             });
+//             res.write("<h1>" + fetchedData + "</h1>");
+//         })
+//         .catch((err) => console.log(err));
+// });
+
+// app.get("/", function (req, res) {
+//     axios
+//         .get("https://api.publicapis.org/entries")
+//         .then((response) => {
+//             let fetchedData = [];
+//             response.data.entries.map((item) => {
+//                 fetchedData.push(item);
+//             });
+//             res.write("<h1>" + fetchedData + "</h1>");
+//         })
+//         .catch((err) => console.log(err));
+// });
+
+// app.listen(3000, function () {
+//     console.log("Server is running on port 3000");
+// });
